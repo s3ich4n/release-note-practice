02c7a89596c07f0acf4224510f23491f49ef10db
Seongeun Yu <s3ich4n@gmail.com>
2021-12-04 18:17:41 +0900

    feat: initial commit (2)
    Done
    + 프로젝트 구조 잡기

---
+++ config/settings/production.py
@@ -0,0 +1,12 @@
+#
+# Production 환경 관련
+#
+# @author      Seongeun Yu (s3ich4n@gmail.com)
+# @date        2021/12/04 17:28 created.
+# @modified    2021/12/04 17:28 modified.
+# @copyright   MIT License
+#
+
+
+from .base import *  # noqa
+from .base import env
